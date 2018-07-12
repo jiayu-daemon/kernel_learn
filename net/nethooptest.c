d3d2c5eb91f63a376044ce3628ae134fdb79d783
jiayu <jiayu_daemon@163.com>
2018-07-12 14:05:55 +0800

    changed

--- net/nethooptest.c
+++ net/nethooptest.c
@@ -1,50 +1,44 @@
-#include <linux/moudle>
-#include <linux/kernel>
-#include <skbuff.h>
-//此处省略一些头文件
-
-static unsigned int my_hook(unsigned int hooknum,
-                   struct sk_buff *skb,
-                   const struct net_device *in,
-                   const struct net_device *out,
-                   int (*okfn)(struct sk_buff *));
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+ 
+ 
+static struct nf_hook_ops nfho;
+ 
+ 
+static unsigned int hook_func(unsigned int hooknum,
+                       struct sk_buff *skb,
+                       const struct net_device *in,
+                       const struct net_device *out,
+                       int (*okfn)(struct sk_buff *))
 {
-    struct iphdr *ip;
-    struct udphdr *udp;
-
-    if(!skb)
-        return NF_ACCEPT;
-    if(skb->protocol != htons(0x0800)) //抓ip包（排除arp包）
-        return NF_ACCEPT
-    ip = ip_hdr(skb);
-    if(ip->protocol != 17)// 抓取udp数据包
-        return NFACCEPT;
-
-    //udp = udp_hdr(skb); //这个函数虽然有，却不一定能得到正确的结果，这取决于钩子函数的挂载点和优先级
-    udp = (struct udphdr *)(ip+1);
-    if(ntohs(udp->dest) != 53) //过滤udp目的端口为53端口的数据
-        return NFACCEPT; 
-
-    return NF_DROP;
+    return NF_ACCEPT;
 }
-
-struct nf_hook_ops my_ops = {
-    .list = {NULL, NULL},
-    .hook = my_hook,
-    .pf = PF_INET,
-    .hooknum = NF_INET_PRE_ROUTING,
-    .priority = NF_IPPRI_FIRST+1
-}          
-
-static int __init m_init(void)
+ 
+ 
+static int nf_init(void)
 {
-    nf_register_hook(&my_ops);
+    nfho.hook = hook_func;
+    nfho.hooknum  = NF_INET_PRE_ROUTING; 
+    nfho.pf       = PF_INET;
+    nfho.priority = NF_IP_PRI_FIRST;
+ 
+ 
+    nf_register_hook(&nfho);
+ 
+ 
+    return 0;
 }
-
-static void __exit m_exit(void)
+ 
+ 
+static void  nf_cleanup(void)
 {
-    nf_unregister_hook(&my_ops);
+    nf_unregister_hook(&nfho);
 }
-
-module_init(m_exit);
-module_exit(m_exit);
+ 
+ 
+module_init(nf_init);
+module_exit(nf_cleanup);
+MODULE_AUTHOR("djwow");
+MODULE_LICENSE("GPL");
